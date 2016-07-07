453531e750290cee65cc25215c3f154d36905113
Jason Shen <sikajs@gmail.com>
2016-07-07 16:10:07 +0800

    Mark As Complete ability

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   root "todo_lists#index"
 
   resources :todo_lists do
-    resources :todo_items
+    resources :todo_items do
+    	member do
+    		patch :complete
+    	end
+    end
   end
   
 end
